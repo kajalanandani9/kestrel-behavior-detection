a97e7c05537f39072c193f55e640ce4d5fa32126
kajalanandani9 <80969439+kajalanandani9@users.noreply.github.com>
2025-07-17 17:11:27 +0200

    utils/metrics.py

---
+++ utils/metrics.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def yolo_to_corners(box):
+    x_center, y_center, w, h = box
+    x1 = x_center - w / 2
+    y1 = y_center - h / 2
+    x2 = x_center + w / 2
+    y2 = y_center + h / 2
+    return [x1, y1, x2, y2]
+
+def compute_iou(box1, box2):
+    xA = max(box1[0], box2[0])
+    yA = max(box1[1], box2[1])
+    xB = min(box1[2], box2[2])
+    yB = min(box1[3], box2[3])
+
+    interArea = max(0, xB - xA) * max(0, yB - yA)
+    box1Area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2Area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+
+    iou = interArea / float(box1Area + box2Area - interArea + 1e-6)  # +epsilon for zero div
+    return iou
